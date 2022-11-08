238e6cc5dde2a0ed2f8446c5af49cf3dd64531da
EgonLiveRasmussen <ecr@live.dk>
2022-11-08 12:50:40 +0100

    Rettet routing

--- Xaminals/AppShell.xaml.cs
+++ Xaminals/AppShell.xaml.cs
@@ -1,9 +1,18 @@
-﻿namespace Xaminals;
+﻿using Xaminals.Views;
+
+namespace Xaminals;
 
 public partial class AppShell : Shell
 {
     public AppShell()
     {
         InitializeComponent();
+        BindingContext = this;
+
+        Routing.RegisterRoute("monkeydetails", typeof(MonkeyDetailPage));
+        Routing.RegisterRoute("beardetails", typeof(BearDetailPage));
+        Routing.RegisterRoute("catdetails", typeof(CatDetailPage));
+        Routing.RegisterRoute("dogdetails", typeof(DogDetailPage));
+        Routing.RegisterRoute("elephantdetails", typeof(ElephantDetailPage));
     }
 }
