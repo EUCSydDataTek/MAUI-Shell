9d428ba68d1cd378f4eec986230391426d731f42
EgonLiveRasmussen <ecr@live.dk>
2022-11-08 12:57:34 +0100

    added routing

--- Xaminals/AppShell.xaml.cs
+++ Xaminals/AppShell.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Input;
+using Xaminals.Views;
 
 namespace Xaminals;
 
@@ -7,8 +8,13 @@ public partial class AppShell : Shell
     public AppShell()
     {
         InitializeComponent();
-
         BindingContext = this;      // nødvendig for at Command binding fungerer
+
+        Routing.RegisterRoute("monkeydetails", typeof(MonkeyDetailPage));
+        Routing.RegisterRoute("beardetails", typeof(BearDetailPage));
+        Routing.RegisterRoute("catdetails", typeof(CatDetailPage));
+        Routing.RegisterRoute("dogdetails", typeof(DogDetailPage));
+        Routing.RegisterRoute("elephantdetails", typeof(ElephantDetailPage));
     }
 
     public ICommand HelpCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
